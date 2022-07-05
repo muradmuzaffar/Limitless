9284bc24d5f0e27688a11ad5e7dc126e70ea30f0
aqilcafarov <63053607+aqilcafarov@users.noreply.github.com>
2022-07-05 17:14:40 +0400

    landing page responsive done

--- static/js/slider.js
+++ static/js/slider.js
@@ -6,8 +6,8 @@ $('.slider_main').slick({
   arrows: true,
   fade: true,
   asNavFor: '.slider_nav',
-  prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-angle-left m-auto"></i></span>',
-  nextArrow: '<span class="next_arrow"><i class="fa-solid fa-angle-right m-auto"></i></span>'
+  prevArrow: '<span class="prev_arrow "><i class="fa-solid fa-angle-left m-auto"></i></span>',
+  nextArrow: '<span class="next_arrow "><i class="fa-solid fa-angle-right m-auto"></i></span>'
 });
 $('.slider_nav').slick({
   slidesToShow: 4,
@@ -61,6 +61,31 @@ $('.landing_slider').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
   arrows: true,
-  prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-angle-left m-auto"></i></span>',
-  nextArrow: '<span class="next_arrow"><i class="fa-solid fa-angle-right m-auto"></i></span>'
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow: '<span class="prev_arrow_l d-none d-md-block"><i class="fa-solid fa-angle-left m-auto"></i></span>',
+  nextArrow: '<span class="next_arrow_l d-none d-md-block"><i class="fa-solid fa-angle-right m-auto"></i></span>',
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
 });

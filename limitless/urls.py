53c40147c3b02bbaa08b561ba7b8e918d8da979b
IsmayilAsadov <ismayil.asadovv@gmail.com>
2022-09-07 15:12:30 +0400

    Update urls.py

--- limitless/urls.py
+++ limitless/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("pagessss.urls")),
+    path('', include("pages.urls")),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
